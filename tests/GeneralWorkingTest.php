5881cb4f0a69ddce9bab3bdaca958e0a49e05cf4
Paul Rijke <paul@rijke.org>
2023-03-28 13:27:29 +0200

    initial instalation of quality tools to be used in this project

---
+++ tests/GeneralWorkingTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class GeneralWorkingTest extends TestCase
+{
+
+    /**
+     * @return void
+     */
+    public function testThatItWorks(): void
+    {
+
+        $this->assertTrue(true);
+
+    }
+}
