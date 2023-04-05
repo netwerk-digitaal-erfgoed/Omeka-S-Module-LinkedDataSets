b3be8a3681150e30ab31ae933dcfa3b4ab653096
Paul Rijke <paul@rijke.org>
2023-04-05 16:18:48 +0200

    basic bootstrap en phpunit.xml

---
+++ test/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+require dirname(dirname(dirname(__DIR__))) . '/bootstrap.php';
+//make sure error reporting is on for testing
+error_reporting(E_ALL);
+ini_set('display_errors', "1");
+
+// Install a fresh database.
+//file_put_contents('php://stdout', "Dropping test database schema…\n");
+//\Omeka\Test\DbTestCase::dropSchema();
+//file_put_contents('php://stdout', "Creating test database schema…\n");
+//\Omeka\Test\DbTestCase::installSchema();
