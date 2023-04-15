4c28ddaa47dfa97715c32c2fa37b962b8a87ae0f
Paul Rijke <paul@rijke.org>
2023-04-15 11:21:59 +0200

    Added check if the distribution is defined

--- src/Application/Service/DistributionService.php
+++ src/Application/Service/DistributionService.php
@@ -5,12 +5,18 @@ declare(strict_types=1);
 namespace LinkedDataSets\Application\Service;
 
 use EasyRdf\Graph;
+use EasyRdf\Resource;
 use LinkedDataSets\Application\Dto\DistributionDto;
 
 final class DistributionService
 {
     public function getDistribution(Graph $graph) {
         $distributionItem = $graph->resourcesMatching('^schema:distribution');
+
+        if (! $distributionItem[0] instanceof Resource) {
+            throw new \Exception('There is no distribution defined');
+        }
+
         $newGraph = $graph::newAndLoad($distributionItem[0]->getUri());
 
         $format = $newGraph->getLiteral( $distributionItem[0]->getUri(), 'schema:encodingFormat')->getValue();
