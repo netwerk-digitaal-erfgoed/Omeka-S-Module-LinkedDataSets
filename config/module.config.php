8447661f63dbc8d4eb140449cd12cd79f800821e
Paul Rijke <paul@rijke.org>
2023-04-18 10:14:53 +0200

    service definition for dsitribution service

--- config/module.config.php
+++ config/module.config.php
@@ -2,6 +2,17 @@
 
 namespace LinkedDataSets;
 
+
+
+use LinkedDataSets\Infrastructure\Services\Factories\DistributionServiceFactory;
+
+return [
+    'service_manager' => [
+        'factories' => [
+            'LDS\DistributionService' => DistributionServiceFactory::class,
+        ]
+    ]
+];
 return [
     'dependencies' => [
         'modules' => [
