0c5505ba155e149eda05a40d28871fddf347b640
Paul Rijke <paul@rijke.org>
2023-04-22 14:46:35 +0200

    helpers for interacting with database api's

--- src/Infrastructure/Helpers/ApiManagerHelper.php
+++ src/Infrastructure/Helpers/ApiManagerHelper.php
@@ -3,38 +3,51 @@
 declare(strict_types=1);
 
 namespace LinkedDataSets\Infrastructure\Helpers;
+
 use LinkedDataSets\Infrastructure\Exception\CatalogResourceTemplateDoesNotExists;
 use Omeka\Api\Manager;
 
 final class ApiManagerHelper
 {
     private const DATACATALOG_LABEL = 'LDS Datacatalog';
-    
+
     protected $api;
-  public function __construct(Manager $api)
-  {
-      $this->api = $api;
-  }
 
-  public function getDatacatalogTemplateId()
-  {
-      $templates = $this->api->search('resource_templates')
-          ->getContent()
-      ;
+    public function __construct(Manager $api)
+    {
+        $this->api = $api;
+    }
 
-      foreach ($templates as $template) {
-          if ($template->label() === self::DATACATALOG_LABEL) {
-              $templateId = $template->id();
-              break;
-          }
-      }
+    private function getDatacatalogTemplateId()
+    {
+        $templates = $this->api->search('resource_templates')
+            ->getContent()
+        ;
 
-      if (! $templateId) {
-          throw new CatalogResourceTemplateDoesNotExists(
-            'There\'s no resource template for Datacatalogs'  
-          );
-      }
+        foreach ($templates as $template) {
+            if ($template->label() === self::DATACATALOG_LABEL) {
+                $templateId = $template->id();
+                break;
+            }
+        }
 
-      return $templateId;
-  }
+        if (!$templateId) {
+            throw new CatalogResourceTemplateDoesNotExists(
+                'There\'s no resource template for Datacatalogs'
+            );
+        }
+
+        return $templateId;
+    }
+
+    public function getDatacatalogs() {
+        $catalogResourceTemplateId = $this->getDatacatalogTemplateId();
+
+        $data = ['resource_template_id' => [$catalogResourceTemplateId]];
+
+        return $this->api
+            ->search('items', $data)
+            ->getContent()
+        ;
+    }
 }
